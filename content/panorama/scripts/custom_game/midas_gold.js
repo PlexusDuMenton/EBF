33a9c97ee5d46fc0d74ab944456bf40614978f42
Huard Ouadi <FrenchDeathDesigne@gmail.com>
2018-04-16 16:35:53 +0200

    moar

--- content/panorama/scripts/custom_game/midas_gold.js
+++ content/panorama/scripts/custom_game/midas_gold.js
@@ -4,7 +4,8 @@ var team = Entities.GetTeamNumber( PlayerEntityIndex )
 $('#'+"midas_gold").visible = false;
 $('#'+"midas_gold_open").visible = false;
 $('#'+"midas_gold_close").visible = false;
-var open = true
+$.Msg('Team ID',team)
+GameEvents.Subscribe( "create_midas_display", create_gold_display)
 	function create_gold_display()
 	{
 		if (team < 3)
@@ -13,24 +14,9 @@ var open = true
 			$('#'+"midas_gold").visible = true;
 		}
 	}
-Updatemidas()
-function Updatemidas(){
-	$.Schedule(0.025, Updatemidas);
-	data = CustomNetTables.GetTableValue( "midas", "total")
-	if (typeof data != 'undefined') {
-	if (open){
-		open_gold()
-	}else{
-		close_gold()
-	}
-		$('#'+"midas_gold_earned").text = data.gold;
-	}
-	
-}
-	
+GameEvents.Subscribe( "Update_Midas_Gold", update_gold)
 	function open_gold()
-	{	
-		open = true
+	{
 		$('#'+"midas_gold_open").visible = false;
 		$('#'+"midas_gold_close").visible = true;
 		$('#'+"midas_gold").visible = true;
@@ -38,10 +24,15 @@ function Updatemidas(){
 	
 	function close_gold()
 	{
-		open = false
 		$('#'+"midas_gold_open").visible = true;
 		$('#'+"midas_gold_close").visible = false;
 		$('#'+"midas_gold").visible = false;
+	}
+	
+	function update_gold(values)
+	{
+		$.Msg('update_gold_earned : ',values.gold);
+		$('#'+"midas_gold_earned").text = values.gold;
 	}
 
 		
--- content/panorama/scripts/custom_game/option.js
+++ content/panorama/scripts/custom_game/option.js
@@ -1,30 +1,12 @@
 var ID = Players.GetLocalPlayer()
 var PlayerEntityIndex = Players.GetPlayerHeroEntityIndex(ID)
 var team = Entities.GetTeamNumber( PlayerEntityIndex )
-$("#DPS_main").visible = false;
+$("#hp_bar_general").visible = false;
+$("#hp_bar_shield").visible = false;
 		var HB_State = true;
-		var dps_State = false;
-		$('#'+"option_unmute_sound").visible = false; 
 		$('#'+"option_close").visible = false; 
 		$('#'+"option_general").visible = false; 
 
-		function mute_sound()
-		{
-			$("#option_mute_sound").visible = false;
-			$("#option_unmute_sound").visible = true;
-			var iPlayerID = Players.GetLocalPlayer();
-			$.Msg('Player ID mute ');
-			GameEvents.SendCustomGameEventToServer( "mute_sound", { pID: iPlayerID} );
-		}
-		
-		function unmute_sound()
-		{
-			$("#option_mute_sound").visible = true;
-			$("#option_unmute_sound").visible = false;
-			var iPlayerID = Players.GetLocalPlayer();
-			$.Msg('Player ID unmute ');
-			GameEvents.SendCustomGameEventToServer( "unmute_sound", { pID: iPlayerID} );
-		}
 		function switch_HB()
 		{
 			if (HB_State)
@@ -32,28 +14,16 @@ $("#DPS_main").visible = false;
 				HB_State = false;
 				$("#hp_bar_general").visible = false;
 				$('#'+"text_HB").text = "Enable Custom HealthBar";
+				var iPlayerID = Players.GetLocalPlayer();
+				GameEvents.SendCustomGameEventToServer( "Health_Bar_Command", { Enabled: false , pID: iPlayerID} );
 			}
 			else
 			{
 				HB_State = true;
 				$("#hp_bar_general").visible = true;
 				$('#'+"text_HB").text = "Disable Custom HealthBar";
-				
-			}
-		}
-		function switch_dps()
-		{
-			if (dps_State)
-			{
-				dps_State = false;
-				$("#DPS_main").visible = false;
-				$('#'+"text_DPS").text = "Enable DPS Meter";
-			}
-			else
-			{
-				dps_State = true;
-				$("#DPS_main").visible = true;
-				$('#'+"text_DPS").text = "Disable DPS Meter";
+				var iPlayerID = Players.GetLocalPlayer();
+				GameEvents.SendCustomGameEventToServer( "Health_Bar_Command", { Enabled: true , pID: iPlayerID} );
 			}
 		}
 		function open_option()
@@ -70,51 +40,41 @@ $("#DPS_main").visible = false;
 		}
 
 
-
-UpdateHB()
-
-function UpdateHB(){
-$.Schedule(0.025, UpdateHB);
-
-	data = CustomNetTables.GetTableValue( "HB", "HB")
-	if (typeof data != 'undefined') {
-		if (data.HP == 0 ){ $("#hp_bar_general").visible = false}
-		$("#hp_bar_parent_health").style.clip = "rect( 0% ," + ((data.HP/data.Max_HP)*77.3+22.7) + "%" + ", 100% ,0% )";
-		$("#hp_bar_current").text = Number((data.HP).toFixed(0));
-		$("#hp_bar_total").text = Number((data.Max_HP).toFixed(0));
-		$("#hp_bar_name").text = "#"+data.Name;
-		if (data.Max_MP != 0){
-			$("#hp_bar_parent_mana").style.clip = "rect( 0% ," + ((data.MP/data.Max_MP)*63.1+27.0) + "%" + ", 100% ,0% )";
-		} else{
-			$("#hp_bar_parent_mana").style.clip = "rect( 0% , 100%, 100% ,0% )";
+GameEvents.Subscribe( "Update_Health_Bar", update_hp_bar)
+GameEvents.Subscribe( "Close_Health_Bar", close_HB)
+GameEvents.Subscribe( "Open_Health_Bar", open_HB)
+GameEvents.Subscribe( "disactivate_shield", desactivate_shield)
+GameEvents.Subscribe( "activate_shield", activate_shield)
+	function close_HB()
+	{
+		if (team < 3)
+		{
+			$("#hp_bar_general").visible = false;
 		}
-		if (data.shield == 1){
-			$("#hp_bar_shield").visible = true;
-		}else { $("#hp_bar_shield").visible = false;}
-		
-		if (HB_State) {
-			$("#hp_bar_general").visible = true;
-		} else { $("#hp_bar_general").visible = false}
+	}
+	function open_HB()
+	{
+		if (team < 3)
+			{
+				$("#hp_bar_general").visible = true;
+			}
+	}
 
-	} else { $("#hp_bar_general").visible = false}
-}
-
-
-	UpdateDPS()
-
-function UpdateDPS(){
-	$.Schedule(0.025, UpdateDPS);
-	key = "player_" + ID.toString()
-	data = CustomNetTables.GetTableValue( "Damage", key)
-	if (typeof data != 'undefined') {
-		$("#damage").text = Number((data.Hero_Damage).toFixed(0));;
-		$("#TD").text = Number((data.Team_Damage).toFixed(0));
-		var time = CustomNetTables.GetTableValue( "time", "time").time
-		var dps = (data.Hero_Damage/(time - data.First_hit))
-		$("#DPS").text = Number((dps).toFixed(2));
+	function desactivate_shield()
+	{
+		$("#hp_bar_shield").visible = false;
 	}
 	
-}
-
+	function activate_shield()
+	{
+		$("#hp_bar_shield").visible = true;
+	}
+	
+	function update_hp_bar(arg)
+	{
+		$("#hp_bar_parent_health").style.clip = "rect( 0% ," + ((arg.current_life_disp/arg.total_life_disp)*96.13+1.83) + "%" + ", 100% ,0% )";
+		$("#hp_bar_current").text = arg.current_life;
+		$("#hp_bar_name").text = $.Localize("#"+arg.name)
+	}
 		
 		
